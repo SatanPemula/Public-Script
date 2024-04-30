11049e52ed93e4882bcafb98576f610d2eafa931
SatanLua <133543243+SatanPemula@users.noreply.github.com>
2024-04-30 09:28:23 +0700

    Update satan.lua

--- satan.lua
+++ satan.lua
@@ -1,1 +1,10 @@
-doLog("This You Discord ID : ".. getDiscordID()) 
+discord = getDiscordID()
+function log(str) 
+  sendVariant({
+      [0] = "OnTextOverlay", 
+      [1] = str, 
+    }) 
+end
+
+log(" Hello User ! ") 
+log(discord)
