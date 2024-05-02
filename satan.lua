5ed71237ce2eafbe2165bcd4d036bc8bbda456a3
monarchSatan <133543243+SatanPemula@users.noreply.github.com>
2024-05-02 18:15:21 +0700

    Update satan.lua

--- satan.lua
+++ satan.lua
@@ -1,96 +1,34 @@
--- GentaHax Custom API For Creative Ps
-hax = {}
-hax.owner = "`4[ S  x  T  x  N ]`` "
-hax.hook = AddHook
-
-function log(str) 
-  logToConsole(hax.owner.. str) 
-end
-
-function overText(str)
+discord = getDiscordID() 
+function logTime(satan, satanSleep) 
+  logToConsole("`9Script Loading...")
   sendVariant({
-      [0] = "OnTextOverlay", 
-      [1] = str, 
-    }
-  ) 
+    [0] = "OnSetMissionTimer", 
+    [1] = satan, 
+  },-1)
+sleep(satanSleep)
+sendVariant({[0] = "OnEndMission", }, -1) 
 end
+logTime(3, 3000)
 
-function drop(a, b) 
-  sendPacket(2, "action|dialog_return\ndialog_name|drop\nitem_drop|"..a.."|\nitem_count|"..b.."\n\n")
-end
-
-function wear(x) 
-  sendPacketRaw(false, 
-    {
-      type = 10,
-      value = x, 
-    }
-  ) 
-end
-
-
-hax.hook("OnTextPacket", "CommandList",function(type, str) 
-    if str:find("/dw (%d+)") then
-      local amt = str:match("/dw (%d+)") 
-      wear(1796) 
-      drop(242, amt)
-      log("`9Dropped `#".. amt .. "`` World lock") 
-      return true
-    end
-    if str:find("/dd (%d+)") then
-      local amt = str:match("/dd (%d+)") 
-      wear(7188) 
-      drop(1796, amt)
-      log("`9Dropped `#".. amt .. "`` Diamond lock") 
-      return true
-    end
-    if str:find("/db (%d+)") then
-      local amt = str:match("/db (%d+)")
-      drop(7188, amt)
-      log("`9Dropped `#".. amt .. "`` Blue Gem lock") 
-      return true
-    end
-    if str:find("/ping") then
-      local ping = getLocal().ping
-      text = ""
-      if ping >= 400 then
-        text = "`4"
-      elseif ping >= 200 then
-        text = "`9"
-      else
-        text = "`2"
-        log("Current Ping : ".. text .. ping) 
-      end
-      return true
-    end
-    if str:find("/daw") then
-      for _, e in pairs(getInventory()) do
-        if e.id == 242 and e.id == 1796 and e.id == 7188 then
-          drop(e.id, e.amount) 
-          log("`2ALL IN") 
-        end
-      end
-      return true
-    end
-    return false
-  end
-)
-hax.hook("OnVarlist","Kontrol", function(var) 
-    if var[0] == "OnConsoleMessage" then
-      log(var[1]) 
-      return true
-    end
-    if var[0] == "OnDialogRequest" and var[1]:find("item_drop") then
-      if var[1]:find("World Lock") or var[1]:find("Diamond Lock") or var[1]:find("Blue Gem Lock") then
-        return true
-      end
-    end
-return false
-  end
-) 
-
-log("`4Costume API Loaded!") 
-sleep(200) 
-log("Hello ".. getLocal().name.. "\nDiscordUID : ".. getDiscordID().. "\nUserID : ".. getLocal().userId) 
-sleep(200) 
-overText("Have Fun!!!!!!") 
+opening = [[
+add_label_with_icon|big|`7Account Profile``|left|12436|
+add_textbox|Hallo ]]..getLocal().name ..[[`` Ini Adalah Account Profile Kamu :3|
+add_textbox|=========================``|left|
+add_label_with_icon|small|`0UID : `b]]..getLocal().userId..[[|left|482|
+add_label_with_icon|small|`0GENTA UID : `b]]..getDiscordID()..[[|left|482|
+add_label_with_icon|small|`0U Standing At `9]]..getWorld().name..[[|left|482|
+add_label_with_icon|small|`0Your Position : `9[ ]]..(getLocal().pos.x//32)..[[ - ]]..(getLocal().pos.y//32)..[[ ]|left|482|
+add_textbox|=========================``|left|
+add_spacer|small|
+text_scaling_string|satankontoljzsj|
+add_button_with_icon|clog|`cChange Log|staticGreyFrame|6128||
+add_button_with_icon|cmd|`cCommand List|staticGreyFrame|32||
+add_button_with_icon|helper|`cHelper Status|staticGreyFrame|1796||
+add_button_with_icon||END_LIST|noflags|0||
+add_spacer|small||
+end_dialog|fakedial|Close||
+]]
+a = {}
+a[0] = "OnDialogRequest"
+a[1] = opening
+sendVariant(a, -1, 200)
